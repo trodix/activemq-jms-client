2606b30376a73fdbca57a66fb681db76ceaba14e
Sébastien Vallet <sebastien.vallet89@gmail.com>
2022-07-28 21:40:04 +0200

    initial commit

---
+++ src/main/java/com/trodix/jmsclient/JmsClientApplication.java
@@ -0,0 +1,13 @@
+package com.trodix.jmsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsClientApplication.class, args);
+	}
+
+}
